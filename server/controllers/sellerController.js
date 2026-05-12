e692ca3957533cc7e45961b36fb38b1367480027
Rohith <kantubhuktharohith@gmail.com>
2026-05-13 01:21:44 +0530

    fix: hardcode cross-domain cookie settings for Vercel

--- server/controllers/sellerController.js
+++ server/controllers/sellerController.js
@@ -12,8 +12,8 @@ export const sellerLogin = async (req, res) =>{
 
         res.cookie('sellerToken', token, {
                 httpOnly: true, 
-                secure: process.env.NODE_ENV === 'production', 
-                sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict', 
+                secure: true, 
+                sameSite: 'none', 
                 maxAge: 7 * 24 * 60 * 60 * 1000, 
                 });
 
@@ -44,8 +44,8 @@ export const sellerLogout = async (req, res)=>{
     try {
         res.clearCookie('sellerToken', {
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? 'none' :'strict',
+            secure: true,
+            sameSite: 'none',
         });
         return res.json({ success: true, message: "Logged Out" })
     } catch (error){
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -24,8 +24,8 @@ export const register = async (req,res)=>{
 
             res.cookie('token', token, {
                 httpOnly: true, 
-                secure: process.env.NODE_ENV === 'production', 
-                sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict', 
+                secure: true, 
+                sameSite: 'none', 
                 maxAge: 7 * 24 * 60 * 60 * 1000, 
                 })
                 return res.json({success: true, user: {_id: user._id, email: user.email, name: user.name, cartItems: user.cartItems}})
@@ -57,8 +57,8 @@ export const login = async (req, res)=>{
 
             res.cookie('token', token, {
                 httpOnly: true, 
-                secure: process.env.NODE_ENV === 'production', 
-                sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict', 
+                secure: true, 
+                sameSite: 'none', 
                 maxAge: 7 * 24 * 60 * 60 * 1000, 
                 })
                 return res.json({success: true, user: {_id: user._id, email: user.email, name: user.name, cartItems: user.cartItems}})
@@ -103,8 +103,8 @@ export const logout = async (req, res)=>{
     try {
         res.clearCookie('token', {
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? 'none' :'strict',
+            secure: true,
+            sameSite: 'none',
         });
         return res.json({ success: true, message: "Logged Out" })
     } catch (error){

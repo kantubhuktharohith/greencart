8f11742bf6ee2565715325ce22e78371c2f2a5ce
Rohith <kantubhuktharohith@gmail.com>
2026-05-13 01:51:16 +0530

    fix: mobile view login button visibility and my-orders navigation link

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -54,7 +54,20 @@ const Navbar = () => {
                     </div>
                 )}
             </div>
-                <div className='flex items-center gap-6 sm:hidden'>
+                <div className='flex items-center gap-4 sm:hidden'>
+                {!user ? (
+                    <button onClick={()=> setShowUserLogin(true)} className="cursor-pointer px-4 py-1.5 bg-primary hover:bg-primary-dull transition text-white rounded-full text-sm">
+                        Login
+                    </button>
+                ) : (
+                    <div className='relative group'>
+                        <img src={assets.profile_icon} className='w-8' alt="" />
+                        <ul className='hidden group-hover:block absolute top-10 right-0 bg-white shadow border border-gray-200 py-2.5 w-30 rounded-md text-sm z-40'>
+                            <li onClick={()=> navigate("my-orders")} className='p-1.5 pl-3 hover:bg-primary/10 cursor-pointer'>My Orders</li>
+                            <li onClick={logout} className='p-1.5 pl-3 hover:bg-primary/10 cursor-pointer'>Logout</li>
+                        </ul>
+                    </div>
+                )}
                 <div onClick={()=> navigate("/cart")} className="relative cursor-pointer">
                     <img src={assets.nav_cart_icon} alt='cart' className='w-6 opacity-80'/>
                     <button className="absolute -top-2 -right-3 text-xs text-white bg-primary w-[18px] h-[18px] rounded-full">{getCartCount()}</button>
@@ -70,7 +83,7 @@ const Navbar = () => {
                 <NavLink to="/" onClick={()=> setOpen(false)}>Home</NavLink>
                 <NavLink to="/product" onClick={()=> setOpen(false)}>All Product</NavLink>
                 {user &&
-                <NavLink to="/products" onClick={()=> setOpen(false)}>My Orders</NavLink>
+                <NavLink to="/my-orders" onClick={()=> setOpen(false)}>My Orders</NavLink>
                 }
                 <NavLink to="/" onClick={()=> setOpen(false)}>Contact</NavLink>
                 {!user ? (

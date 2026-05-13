ac242a016d4063e22200c37b83903bd404566851
Rohith <kantubhuktharohith@gmail.com>
2026-05-13 13:09:58 +0530

    Fix token precedence in authSeller to prioritize sellerToken over Authorization header

--- server/middlewares/authSeller.js
+++ server/middlewares/authSeller.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken';
 
 const authSeller = async (req, res, next) =>{
-    const sellerToken = req.cookies.sellerToken || req.headers.authorization?.split(' ')[1] || req.headers.sellertoken;
+    const sellerToken = req.cookies.sellerToken || req.headers.sellertoken || req.headers.authorization?.split(' ')[1];
     if(!sellerToken) {
         return res.json({ success: false, message: 'Not Authorized' });
     }

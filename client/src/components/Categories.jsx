95d0782a6af34a865b0e314540276956351486b5
Rohith <kantubhuktharohith@gmail.com>
2026-05-14 04:11:04 +0530

    Update Categories.jsx

--- client/src/components/Categories.jsx
+++ client/src/components/Categories.jsx
@@ -15,7 +15,7 @@ const Categories = () => {
                 <div key ={index} className='group cursor-pointer py-5 px-3 gap-2 rounded-lg flex flex-col justify-center items-center'
                 style={{backgroundColor: category.bgColor}}
                 onClick={()=>{
-                navigate(`/products/${category.path.toLowerCase()}`);
+                navigate(`/product/${category.path.toLowerCase()}`);
                 scrollTo(0,0)
                 }}
                 >
